e8660821b14aa10243580cccc223c387e421be90
moonlighteding <moonlighteding@outlook.com>
2025-02-16 11:54:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i "/helloworld/d" "feeds.conf.default"
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# sed -i "/passwall/d" "feeds.conf.default"
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# sed -i "/MosDSN/d" "feeds.conf.default"
-# echo 'src-git MosDSN https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i "/ssr plus/d" "feeds.conf.default"
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i "/passwall/d" "feeds.conf.default"
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i "/MosDSN/d" "feeds.conf.default"
+echo 'src-git MosDSN https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+sed -i "/OpenClash/d" "feeds.conf.default"
+echo 'https://github.com/vernesong/OpenClash' >>feeds.conf.default
+
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
