be9c8a23fb81deb757581ec10659725db6e378cb
moonlighteding <moonlighteding@outlook.com>
2025-03-21 20:40:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,4 @@ sed -i 's/192.168.1.1/192.168.3.250/g' package/base-files/files/bin/config_gener
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/xopwrt/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/xopwrt/g' package/base-files/files/bin/config_generate
